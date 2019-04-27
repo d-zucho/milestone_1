cb64b54a4a5f7b58fed366106890ede25ed80c02
d-zucho <danny.nico.dev@outlook.com>
2019-04-27 11:32:51 -0700

    first menu

--- main.py
+++ main.py
@@ -2,6 +2,38 @@
 
 
 """"
-[] - tell user options: 'a' - add,
+- must allow to add new movies to collection
+- must allow users to view all the movies in the collection
+- allow user to find movie by any attribute
+
+
+[] - tell user options: 'a' - add, 's' - show, 'f' - find, 'q' - quit program
+[] - what format is a movie ~~~~~list of dicts
+[] - decide where to store movies
+
+- Functions -
+[] - create add_movie
+[] - create show_movie
+[] - create find_movie
+[]
 
 """
+movies = []
+
+
+def menu():
+    user_input = input(
+        "Enter 'a' to add a movie, 'l' to see your movies, and 'f' to find a movie: ")
+
+    while user_input != 'q':
+        if user_input == 'a':
+            add_movie()
+        elif user_input == 'l':
+            show_movie()
+        elif user_input == 'f':
+            find_movie()
+        else:
+            print("Entry not applicable. Try agaom")
+
+
+def add_movie():
